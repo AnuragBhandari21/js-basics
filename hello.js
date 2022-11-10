3db8f24f78064d530071cff44170aede3e6d93d5
AnuragB <as0069315@gmail.com>
2022-11-10 08:58:44 +0000

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,7 @@
+const onelinerJoke = require('one-liner-joke');
+/*
+The variable getRandomJoke will contain a random joke with a format:
+{"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+*/
+const getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke)
